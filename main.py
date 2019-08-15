e19682ce49f3609b358dd4c03c8eb612d929b5ce
VladislavEkimtcov <ekimtcov.v@gmail.com>
2019-08-15 00:27:42 -0500

    continued development

--- main.py
+++ main.py
@@ -2,29 +2,44 @@ import jira_utils as ju
 from jiracmd import Jira
 from zeep import client as c
 from zeep.wsse.username import UsernameToken
+import configparser
 
+# read config and get primavera login info
+parser = configparser.ConfigParser()
+with open('login') as configfile:
+    parser.read_file(configfile)
+primadict=parser['primavera-section']
+primauser=primadict['user']
+primapasswd=primadict['passwd']
+primaserver=primadict['server']
+
+# init jira and primavera connections
 jcon = Jira('jira-section')
-
-# make request to primavera
-# convert json output to an in-memory table
-# loop the following code
 # ju.create_ticket('jira-section', jcon.user, ticket=None, parent="5", summary='summary', description='descript', project='LSSTTST')
 
+# Enterprise Project Structure
 request_data = {
-    'Field': 'ActivityId'
-}
+    'Field': 'Name'}
+wsdl_url = 'https://uofi-stage-p6.oracleindustry.com/p6ws/services/EPSService?wsdl'
+soap_client = c.Client(wsdl_url, wsse=UsernameToken(primauser, primapasswd))
+# with soap_client.settings(raw_response=False):
+#     api_result = soap_client.service.ReadEPS(**request_data)
+pass
 
+# ActivityStepService
+request_data = {
+    'Field': ['ActivityName', 'ActivityId']}
 wsdl_url = 'https://uofi-stage-p6.oracleindustry.com/p6ws/services/ActivityStepService?wsdl'
-soap_client = c.Client(wsdl_url, wsse=UsernameToken("EKIMTCOV", ""))
-with soap_client.settings(raw_response=True):
-    weather_api_result = soap_client.service.ReadActivitySteps(**request_data)
-deer = dir(weather_api_result)
+soap_client = c.Client(wsdl_url, wsse=UsernameToken(primauser, primapasswd))
+with soap_client.settings(raw_response=False):
+    api_result = soap_client.service.ReadActivitySteps(**request_data)
+pass
 
-attr_dump = []
-for attrib in deer:
-    x = getattr(weather_api_result, attrib)
-    attr_dump.append(x)
-    pass
-
-print(weather_api_result.__dict__)
-print(weather_api_result.__str__)
+# ActivityService
+request_data = {
+    'Field': 'WBSName'}
+wsdl_url = 'https://uofi-stage-p6.oracleindustry.com/p6ws/services/ActivityService?wsdl'
+soap_client = c.Client(wsdl_url, wsse=UsernameToken(primauser, primapasswd))
+with soap_client.settings(raw_response=False):
+    api_result = soap_client.service.ReadActivities(**request_data)
+pass
