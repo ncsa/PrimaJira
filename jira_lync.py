64b94e0fba4ec2b652edab3778f9cc6743b86212
Vladislav Ekimtcov <ekimtcov.v@gmail.com>
2019-10-14 15:36:18 -0500

    CHANGES: jira_lync.py * added step sync * added exception handling, especially for missing tickets

--- jira_lync.py
+++ jira_lync.py
@@ -6,6 +6,7 @@ import configparser
 from jiracmd import Jira
 import shlog
 import jira_utils as ju
+import jira
 
 
 if __name__ == '__main__':
@@ -33,11 +34,27 @@ if __name__ == '__main__':
     # build two dicts with actid to jira ids
     ncsa_tickets = m.get_activity_tickets(primaserver, primauser, primapasswd, 'ncsa')
     lsst_tickets = m.get_activity_tickets(primaserver, primauser, primapasswd, 'lsst')
-
+    ncsa_step_tickets = m.get_step_tickets(primaserver, primauser, primapasswd, 'ncsa')
+    lsst_step_tickets = m.get_step_tickets(primaserver, primauser, primapasswd, 'lsst')
 
     for act in activities:
         # activity must exist both in ncsa and lsst, or there's quite simply nothing to link
         if act in ncsa_tickets.keys() and act in lsst_tickets.keys():
             # print(ncsa_tickets[act] + '//' + lsst_tickets[act])
-            ju.link_second_jira(jcon, ncsa_tickets[act], lsst_tickets[act], 'https://jira.lsstcorp.org')
-            # add step linking too
+            try:
+                ju.link_second_jira(jcon, ncsa_tickets[act], lsst_tickets[act], 'https://jira.lsstcorp.org')
+                shlog.normal('Success linking ' + ncsa_tickets[act] + ' and ' + lsst_tickets[act])
+            except (jira.exceptions.JIRAError) as e:
+                # jira failures mean we can't sync anything
+                shlog.normal('Jira operation faled with error:')
+                print(e)
+        activity_steps = m.step_list_filter(steps, act)
+        for step in activity_steps:
+            if step in ncsa_step_tickets.keys() and step in lsst_step_tickets.keys():
+                try:
+                    ju.link_second_jira(jcon, ncsa_step_tickets[step], lsst_step_tickets[step], 'https://jira.lsstcorp.org')
+                    shlog.normal('Success linking ' + ncsa_step_tickets[step] + ' and ' + lsst_step_tickets[step])
+                except (jira.exceptions.JIRAError) as e:
+                    # jira failures mean we can't sync anything
+                    shlog.normal('Jira operation faled with error:')
+                    print(e)
